b48afc7a6cf22d93e5b3b930147eead738be768b
Kkals11 <jonahkkalvan@gmail.com>
2025-03-11 13:45:08 -0700

    checked 3

--- our_last/src/components/ClubPage.js
+++ our_last/src/components/ClubPage.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import DataScienceClub from '../clubs/DataScienceClub';
-import CybersecurityClub from '../clubs/CybersecurityClub';
-import RoboticsClub from '../clubs/RoboticsClub';
+import React from "react";
+import { useParams } from "react-router-dom";
+import DataScienceClub from "../clubs/DataScienceClub";
+import CybersecurityClub from "../clubs/CybersecurityClub";
+import RoboticsClub from "../clubs/RoboticsClub";
 
 const clubComponents = {
   datascience: DataScienceClub,
@@ -10,18 +10,20 @@ const clubComponents = {
   robotics: RoboticsClub,
 };
 
+const ClubNotFound = () => (
+  <div className="club-page">
+    <header className="club-header">
+      <h1>Club Not Found</h1>
+    </header>
+    <section className="club-section">
+      <p>Sorry, we couldn’t find that club. Please check the URL or return to the dashboard.</p>
+    </section>
+  </div>
+);
+
 function ClubPage() {
   const { clubName } = useParams();
-  const ClubComponent = clubComponents[clubName.toLowerCase()] || (() => (
-    <div className="club-page">
-      <header className="club-header">
-        <h1>Club Not Found</h1>
-      </header>
-      <section className="club-section">
-        <p>Sorry, we couldn’t find that club. Please check the URL or return to the dashboard.</p>
-      </section>
-    </div>
-  ));
+  const ClubComponent = clubComponents[clubName?.toLowerCase()] || ClubNotFound;
 
   return <ClubComponent />;
 }
