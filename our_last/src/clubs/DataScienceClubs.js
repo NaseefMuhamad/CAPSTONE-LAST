7a10d87bb0c1ff51b53c1c847e186dfc1de32d1d
Jada-Fs <jadafrancis208@gmail.com>
2025-03-11 16:40:55 +0300

    Update DataScienceClubs.js

---
+++ our_last/src/clubs/DataScienceClubs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function   (){
+    return (
+        <div>
+            <h1>Welcome to the Robotics Project</h1>
+            <p>Robotics is a revolutionising industries by significantly enhancing efficiency, precision, and safety. From manufacturing to healthcare, robotics technology is transforming operational processes, driving innovation, and creating unprecedented opportunities for advancement
+                   .</p>
+        </div>
+    );
+};
+
+export default HomePage;
