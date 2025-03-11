79911189c1edf108e54b21c315c4263e83dbb6e9
NaseefMuhamad <naseefmuhamad277@gmail.com>
2025-03-11 20:18:46 +0300

    CHAMA

--- our_last/src/App.js
+++ our_last/src/App.js
@@ -1,14 +1,17 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar'; // Imported and now used
 import Welcome from './components/WelcomePage';
 import Dashboard from './components/Dashboard';
 import ClubPage from './components/ClubPage';
-import './styles/App.css'; 
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar /> {/* Added here to render the navbar */}
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/dashboard" element={<Dashboard />} />
